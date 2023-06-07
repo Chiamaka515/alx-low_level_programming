f3f55c5a2fd2949a5776fe5f8a84c39db6913048
Chiamaka515 <okaforsandra515@gmail.com>
2023-06-06 18:40:34 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
-	if (!h)
-		return (0);
 	while (h)
 	{
-		printf("%i\n", h->n);
-		nodes++;
+		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
-	return (nodes);
+	return (count);
 }
