7ccf3ba5c49e700960de9f12bbde81407456f9e1
Chiamaka515 <okaforsandra515@gmail.com>
2023-06-13 13:16:23 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,28 +11,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
-
-	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+	char *buf;
+	ssize_t o;
+	ssize_t w;
+	ssize_t r;
 
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	if (o == -1)
 		return (0);
-	}
+	buf = malloc(sizeof(char) * letters);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-	free(buffer);
+	free(buf);
 	close(o);
-
 	return (w);
 }
